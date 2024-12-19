cee895077767d7a70f338065837ebca9506e5e46
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2024-12-19 19:30:35 +0200

    hm

---
+++ js/index.js
@@ -0,0 +1,35 @@
+// 1
+
+const world = () => {
+      return 'Hello World!'
+// в цій функці явне повернення бо є return
+}
+console.log(world())
+
+// 2
+
+const applyCallbackToEachElement = (arr, callback) => {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.push(callback(arr[i])); 
+    }
+    return result;
+}
+const arr = [1, 2, 3, 4, 5];
+const squareCallback = (num) => num * num;
+
+const result = applyCallbackToEachElement(arr, squareCallback);
+console.log(result);
+
+// 3
+
+const calculateDiscountPrice = (price, discount, callback) => {
+    return callback(price, discount);
+};
+const discountCallback = (price, discount) => {
+    return price - (price * (discount / 100));
+};
+const originalPrice = 1000; 
+const discountPercentage = 20; 
+const discountedPrice = calculateDiscountPrice(originalPrice, discountPercentage, discountCallback);
+console.log(`Ціна зі знижкою: ${discountedPrice} грн`);
